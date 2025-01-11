c7885032686f5925649e0da9ac10b051c1bae359
Hema Phani Sri Jonnala <hemaj22@iitk.ac.in>
2025-01-11 18:22:04 +0530

    Create 12_conclusion.py

---
+++ 12_conclusion.py
@@ -0,0 +1,6 @@
+pivot_table = dataframe.pivot_table(index='listed_in(type)', columns='online_order', aggfunc='size', fill_value=0)
+sns.heatmap(pivot_table, annot=True, cmap="YlGnBu", fmt='d')
+plt.title("Heatmap")
+plt.xlabel("Online Order")
+plt.ylabel("Listed In (Type)")
+plt.show()
